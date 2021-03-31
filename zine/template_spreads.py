a95eae268d148478e44c32e120e6b7d384b712b0
David Ross <david@djr.com>
2021-03-30 21:29:27 -0400

    Create template_spreads.py

---
+++ zine/template_spreads.py
@@ -0,0 +1,23 @@
+# define our constants
+inch = 72
+theWidth = 5.83 * inch 
+theHeight = 8.27 * inch
+bleed = 0.125 * inch
+
+# do i want to see the bleed
+# make this false before exporting
+drawBleed = True
+
+newPage(theWidth*2+bleed*2, theHeight+bleed*2)
+
+with savedState():
+    if drawBleed:
+        stroke(1, 0, 0)
+        fill(None)
+        strokeWidth(.5)
+        rect(bleed, bleed, theWidth, theHeight)
+        rect(theWidth+bleed, bleed, theWidth, theHeight)
+       
+        
+# move to the (0, 0) of the cropped page
+translate(bleed, bleed)
